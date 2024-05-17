7ef70371283db77ecfe61b081010e0355fbe6c3a
SteeleTyler <tylermsteele@outlook.com>
2024-05-17 09:15:17 -0400

    Initial project structure (template)

---
+++ Slow Track/Slow_TrackApp.swift
@@ -0,0 +1,32 @@
+//
+//  Slow_TrackApp.swift
+//  Slow Track
+//
+//  Created by Tyler Steele on 4/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Slow_TrackApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
